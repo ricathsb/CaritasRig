a59568d55a0eadd4075f07b94a2c3055abd7a480
ricathsb <ricat1907111@gmail.com>
2024-11-01 16:14:20 +0700

    update

---
+++ app/src/main/java/com/superbgoal/caritasrig/auth/Functions.kt
@@ -0,0 +1,49 @@
+package com.superbgoal.caritasrig.auth
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+
+@Composable
+fun LoadingButton(
+    modifier: Modifier = Modifier,
+    text: String,
+    isLoading: Boolean = false,
+    coroutineScope: CoroutineScope,
+    onClick: suspend () -> Unit
+) {
+    var loadingState by remember { mutableStateOf(isLoading) }
+
+    Box(modifier = modifier) { // Use Box to overlay loading indicator
+        Button(
+            onClick = {
+                loadingState = true
+                coroutineScope.launch {
+                    onClick()
+                    loadingState = false
+                }
+            },
+            enabled = !loadingState, // Disable button while loading
+            modifier = Modifier.fillMaxWidth() // Ensure button fills the width
+        ) {
+            Text(text)
+        }
+
+        if (loadingState) {
+            CircularProgressIndicator(
+                modifier = Modifier
+                    .align(Alignment.Center) // Center the loading indicator
+                    .size(40.dp) // Set the size of the CircularProgressIndicator
+            )
+        }
+    }
+}
