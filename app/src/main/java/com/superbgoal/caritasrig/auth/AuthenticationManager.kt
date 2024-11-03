cc2efc192b7e348b81a3126073d776ae85d868b0
ferry <ferrypb123pb123@gmail.com>
2024-11-04 05:30:14 +0700

    UI updates

--- app/src/main/java/com/superbgoal/caritasrig/auth/AuthenticationManager.kt
+++ app/src/main/java/com/superbgoal/caritasrig/auth/AuthenticationManager.kt
@@ -189,3 +189,5 @@ sealed interface AuthResponse {
     data object Success : AuthResponse
     data class Error(val message: String) : AuthResponse
 }
+
+
--- app/src/main/java/com/superbgoal/caritasrig/auth/Functions.kt
+++ app/src/main/java/com/superbgoal/caritasrig/auth/Functions.kt
@@ -1,9 +1,12 @@
+// Function.kt
 package com.superbgoal.caritasrig.auth
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -13,22 +16,24 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
-
 
 @Composable
 fun LoadingButton(
     modifier: Modifier = Modifier,
     text: String,
     isLoading: Boolean = false,
+    colors: ButtonColors = ButtonDefaults.buttonColors(),
+    textColor: Color = Color.White,
     coroutineScope: CoroutineScope,
     onClick: suspend () -> Unit
 ) {
     var loadingState by remember { mutableStateOf(isLoading) }
 
-    Box(modifier = modifier) { // Use Box to overlay loading indicator
+    Box(modifier = modifier) {
         Button(
             onClick = {
                 loadingState = true
@@ -37,20 +42,22 @@ fun LoadingButton(
                     loadingState = false
                 }
             },
-            enabled = !loadingState, // Disable button while loading
-            modifier = Modifier.fillMaxWidth() // Ensure button fills the width
+            enabled = !loadingState,
+            modifier = Modifier.fillMaxWidth(),
+            colors = colors
         ) {
-            Text(text)
+            Text(
+                text = text,
+                color = textColor
+            )
         }
 
         if (loadingState) {
             CircularProgressIndicator(
                 modifier = Modifier
-                    .align(Alignment.Center) // Center the loading indicator
-                    .size(40.dp) // Set the size of the CircularProgressIndicator
+                    .align(Alignment.Center)
+                    .size(24.dp)
             )
         }
     }
 }
-
-
