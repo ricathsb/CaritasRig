3ef6dcd47f11e9566c8527fb193d90ad55728a4c
BrianTzr <briantazar@gmail.com>
2024-11-06 20:32:23 +0700

    test

--- app/src/main/java/com/superbgoal/caritasrig/activity/BuildActivity.kt
+++ app/src/main/java/com/superbgoal/caritasrig/activity/BuildActivity.kt
@@ -6,30 +6,42 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.superbgoal.caritasrig.activity.build.CasingActivity
-import com.superbgoal.caritasrig.activity.build.CpuActivity
-import com.superbgoal.caritasrig.activity.build.InternalHardDriveActivity
-import com.superbgoal.caritasrig.activity.build.MemoryActivity
-import com.superbgoal.caritasrig.activity.build.MotherboardActivity
-import com.superbgoal.caritasrig.activity.build.PowerSupplyActivity
-import com.superbgoal.caritasrig.activity.build.VideoCardActivity
 import com.superbgoal.caritasrig.R
+import com.superbgoal.caritasrig.activity.build.CpuActivity
+import com.superbgoal.caritasrig.activity.build.VideoCardActivity
 import com.superbgoal.caritasrig.auth.ProcessorInfo
 import com.superbgoal.caritasrig.auth.VideoCardInfo
 import com.superbgoal.caritasrig.data.model.Processor
@@ -168,7 +180,6 @@ fun BuildScreen(
 }
 
 
-
 @Composable
 fun ComponentCard(
     title: String,
@@ -176,6 +187,8 @@ fun ComponentCard(
     videoCard: VideoCard? = null,
     onAddClick: () -> Unit
 ) {
+    val context = LocalContext.current // Define context here
+
     Card(
         modifier = Modifier
             .fillMaxWidth()
@@ -187,7 +200,6 @@ fun ComponentCard(
             modifier = Modifier
                 .fillMaxWidth()
         ) {
-            // Header abu-abu dengan title
             Box(
                 modifier = Modifier
                     .fillMaxWidth()
@@ -203,31 +215,28 @@ fun ComponentCard(
                     modifier = Modifier.padding(10.dp)
                 )
             }
+
+            if (title == "CPU" && processor != null) {
+                ProcessorInfo(processor)
+            }
+
+            if (title == "GPU" && videoCard != null) {
+                VideoCardInfo(videoCard)
+            }
+
+            Button(
+                onClick = { onAddClick() },
+                modifier = Modifier.padding(8.dp)
             ) {
-                Button(
-                    onClick = {
-                        val intent = Intent(context, activity)
-                        context.startActivity(intent)
-                    },
-                    modifier = Modifier
-                        .padding(8.dp)
-                        .background(Color.Transparent),  // Warna latar belakang transparan
-                    elevation = ButtonDefaults.buttonElevation(0.dp) // Menghapus efek bayangan tombol
-                ) {
-                    Icon(
-                        painter = painterResource(id = R.drawable.add_btn),  // Gambar dari res/drawable/add_btn
-                        contentDescription = "Add Icon",
-                        tint = Color.Unspecified,  // Mempertahankan warna asli ikon
-                        modifier = Modifier.size(18.dp)
-                    )
-                    Spacer(modifier = Modifier.width(8.dp))  // Jarak antara ikon dan teks
-                    Text(text = "Add Component")
-                }
+                Icon(
+                    painter = painterResource(id = R.drawable.add_btn),
+                    contentDescription = "Add Icon",
+                    tint = Color.Unspecified,
+                    modifier = Modifier.size(18.dp)
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(text = "Add Component")
             }
         }
     }
 }
-
-
-
-
