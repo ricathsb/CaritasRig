f5d01a9b45f70177113ce40d4b4d3d8e803da10e
Samuel Sitanggang_125 <samuelsitanggang04@gmail.com>
2024-11-26 20:24:06 +0700

    BenchmarkViewModel dan pager untuk BenchmarkScreen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -108,4 +108,9 @@ dependencies {
 
     implementation(libs.androidx.navigation.compose)
 
+    //pager
+    implementation ("com.google.accompanist:accompanist-pager:0.32.0")
+    implementation ("com.google.accompanist:accompanist-pager-indicators:0.32.0")
+
+
 }
--- app/src/main/java/com/superbgoal/caritasrig/activity/homepage/benchmark/BenchmarkScreen.kt
+++ app/src/main/java/com/superbgoal/caritasrig/activity/homepage/benchmark/BenchmarkScreen.kt
@@ -1,51 +1,33 @@
 package com.superbgoal.caritasrig.activity.homepage.benchmark
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Button
-import androidx.compose.material.ButtonDefaults
-import androidx.compose.material.Card
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.ListItem
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.google.accompanist.pager.*
 import com.superbgoal.caritasrig.R
-import com.superbgoal.caritasrig.activity.homepage.buildtest.component.ProcessorList
 import com.superbgoal.caritasrig.data.loadItemsFromResources
 import com.superbgoal.caritasrig.data.model.component.Processor
 import com.superbgoal.caritasrig.data.model.component.VideoCard
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalPagerApi::class)
 @Composable
 fun BenchmarkScreen(navController: NavController) {
     val context = LocalContext.current
@@ -64,16 +46,13 @@ fun BenchmarkScreen(navController: NavController) {
         )
     }
 
-    // State to track selected category (Processor or GPU)
-    var selectedCategory by remember { mutableStateOf("Processor") }
-
-    // Determine which list to display based on selectedCategory
-    val displayedItems = if (selectedCategory == "Processor") processors else videoCards
+    val pagerState = rememberPagerState(initialPage = 0)
+    val coroutineScope = rememberCoroutineScope()
 
     Box(
         modifier = Modifier.fillMaxSize()
     ) {
-        // Set background image
+        // Background image
         Image(
             painter = painterResource(id = R.drawable.component_bg),
             contentDescription = null,
@@ -82,46 +61,56 @@ fun BenchmarkScreen(navController: NavController) {
         )
 
         Column {
+            // Tab indicators
             Row(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(vertical = 8.dp),
+                    .padding(top = 10.dp, bottom = 0.dp),
                 horizontalArrangement = Arrangement.Center
             ) {
                 Button(
-                    onClick = { selectedCategory = "Processor" },
+                    onClick = {
+                        coroutineScope.launch {
+                            pagerState.animateScrollToPage(0)
+                        }
+                    },
                     colors = ButtonDefaults.buttonColors(
-                        backgroundColor = if (selectedCategory == "Processor") Color.Blue else Color.Gray
+                        backgroundColor = if (pagerState.currentPage == 0) colorResource(id = R.color.brown) else Color.Gray
                     ),
-                    modifier = Modifier.padding(horizontal = 8.dp)
+                    modifier = Modifier.padding(horizontal = 8.dp).weight(1f)
                 ) {
                     Text(text = "Processor", color = Color.White)
                 }
                 Button(
-                    onClick = { selectedCategory = "GPU" },
+                    onClick = {
+                        coroutineScope.launch {
+                            pagerState.animateScrollToPage(1)
+                        }
+                    },
                     colors = ButtonDefaults.buttonColors(
-                        backgroundColor = if (selectedCategory == "GPU") Color.Blue else Color.Gray
+                        backgroundColor = if (pagerState.currentPage == 1) colorResource(id = R.color.brown) else Color.Gray
                     ),
-                    modifier = Modifier.padding(horizontal = 8.dp)
+                    modifier = Modifier.padding(horizontal = 8.dp).weight(1f)
                 ) {
                     Text(text = "GPU", color = Color.White)
                 }
             }
 
-            // List content
-            Surface(
-                modifier = Modifier.fillMaxSize(),
-                color = Color.Transparent
-            ) {
-                if (selectedCategory == "Processor") {
-                    ProcessorListWithFavorite(processors = processors, navController = navController)
-                } else {
-                    VideoCardListWithFavorite(videoCards = videoCards, navController = navController)
+            // Swipeable Pager
+            HorizontalPager(
+                count = 2,
+                state = pagerState,
+                modifier = Modifier.fillMaxSize()
+            ) { page ->
+                when (page) {
+                    0 -> ProcessorListWithFavorite(processors = processors, navController = navController)
+                    1 -> VideoCardListWithFavorite(videoCards = videoCards, navController = navController)
                 }
             }
         }
     }
 }
+
 
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
